6bb912743b1ee278125f104edf2928e602cdb8f2
tony <tonilela@gmail.com>
2018-06-02 15:01:18 +0200

    finish

--- src/NovaKomponenta.js
+++ src/NovaKomponenta.js
@@ -2,32 +2,45 @@
  * Created by Lela on 31.5.2018..
  */
 import React, { Component } from 'react';
-//import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
+import './App.css';
 
 
 
+class Color extends Component{
+    handleClick=()=>{
+      this.props.onClick(this.props.boja)
+    };
+    render() {
+        return (
 
-const Color =(props)=>{
-console.log(props.boja);
-    return(
-        <div style={{background:props.boja}}></div>
-    );
+            <div className="Color-colum" onClick={this.handleClick} style={{background: this.props.boja}}>
+                {this.props.value}
+            </div>
 
+        );
 
+    }
 
-};
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 class NovaKomponenta extends Component{
     constructor(props) {
         super(props);
         this.state = {
             isLoaded:false,
-            colorBlack: 'blue',
             colorOne:'',
             colorTwo: '',
             colorThree: 'black',
-            text:"",
+            value:'',
+            rand:0,
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.changeColor=this.changeColor.bind(this);
 
     }
     componentDidMount() {
@@ -41,20 +54,51 @@ class NovaKomponenta extends Component{
         .then((json) => this.setState({colorTwo: json['colors'][0]['hex']}));
 }}
 
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
 
+   changeColor=() =>{
+        if(this.state.colorThree==='black'){
+
+            let random=getRandomInt(100);
+            this.setState({ rand :this.state.rand+random+1});
+            if( (this.state.rand %2)===0){
+                this.setState({colorTwo: this.state.colorTwo});
+                console.log(this.state.colorTwo+"druga");
+                this.setState({colorThree: '#'+this.state.colorTwo});
+            }
+            else{
+                this.setState({colorOne: this.state.colorOne});
+                console.log(this.state.colorOne+"prva");
+                this.setState({colorThree: '#'+this.state.colorOne});
+            }
+
+        }
+        else{
+
+            this.setState({colorThree: 'black'});
+        }
+
+
+    }
 
     render(){
 
+        const rules ={
+            color: 'black',
+        };
 
-        const  RealColor = "#"+this.state.colorTwo;
        return(
-           <div style={{background:RealColor}}>
-                       <Color  boja={RealColor} />
-              <div style={{background:RealColor}}>
+           <div >
+               <Color  boja={this.state.colorThree}
+                       value={this.state.value}
+                       onClick={this.changeColor}/>
 
-
-
-                  </div>
+               <input style={rules}
+                      type="text"
+                      value={this.state.value}
+                      onChange={this.handleChange} />
 
            </div>
        );
